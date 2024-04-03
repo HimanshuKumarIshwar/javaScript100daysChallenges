f20f0b62e37f5da148f7bd3a9e8146e59d328e75
Himanshu Kumar <himanshuishwar399@gmail.com>
2024-04-03 23:34:58 +0530

    day7

---
+++ day7/index.js
@@ -0,0 +1,16 @@
+function getMax(arr){
+    
+// let min = arr.reduce((prev, accum) => {
+//     if(accum > prev)return accum;
+//     return prev;
+// })
+// return min;
+
+
+return Math.max(...arr);
+}
+
+
+arr = [1, 5, 3, 9, 2. -4, 100];
+console.log(getMax([-4]));
+
